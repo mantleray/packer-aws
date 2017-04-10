ea9d3c8ffb4944396d11081f8161dc4415777407
S C <stuart@Ss-MacBook-Pro.local>
2017-04-10 12:26:05 +0100

    Init

--- scripts/keys_setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-apt-get -y install curl
-mkdir /home/vagrant/.ssh
-chmod 700 /home/vagrant/.ssh
-curl https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub -O >> /home/vagrant/.ssh/authorized_keys
-chmod 600 .ssh/authorized_keys
-chown -R vagrant:vagrant .ssh
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -eux
 
 # Add vagrant user to sudoers.
-echo "vagrant        ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers
+echo "ubuntu        ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers
 sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
 
 # Disable daily apt unattended updates.
