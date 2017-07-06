9da884d2dd486176251dadb5304ce18980fb2884
mantleray <stuartjcramer@gmail.com>
2017-07-06 13:02:44 +0100

    Update jenkins.sh

--- scripts/jenkins.sh
+++ scripts/jenkins.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
 sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-apt-get -y update && apt-get -y upgrade
+apt-get -y update
+UCF_FORCE_CONFFNEW=YES apt-get upgrade -y
 apt-get -y install openjdk-8-jdk
 apt-get -y install jenkins
